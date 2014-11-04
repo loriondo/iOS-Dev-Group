f5917f911d861d632f35f1717e590da681af2f9c
Lou Oriondo <loriondo@uga.edu>
2014-11-03 23:09:29 -0500

    Missed some files.

---
+++ Quiz2/Quiz2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz2
+//
+//  Created by Lou Oriondo on 11/3/14.
+//  Copyright (c) 2014 ACM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
---
+++ Quiz2/Quiz2Tests/Quiz2Tests.m
@@ -0,0 +1,34 @@
+//
+//  Quiz2Tests.m
+//  Quiz2Tests
+//
+//  Created by Lou Oriondo on 11/3/14.
+//  Copyright (c) 2014 ACM. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Quiz2Tests : XCTestCase
+
+@end
+
+@implementation Quiz2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
